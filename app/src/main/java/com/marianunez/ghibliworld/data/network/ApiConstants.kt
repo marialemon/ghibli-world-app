5a983503c0b7c671770041117fdca6b0a0b91b92
maria.nunez <maria.nunez@wefox.com>
2022-06-08 18:26:59 +0200

    add api constants

---
+++ app/src/main/java/com/marianunez/ghibliworld/data/network/ApiConstants.kt
@@ -0,0 +1,9 @@
+package com.marianunez.ghibliworld.data.network
+
+object ApiConstants {
+    const val FILMS = "/films"
+    const val PEOPLE = "/people"
+    const val LOCATIONS = "/locations"
+    const val SPECIES = "/species"
+    const val VEHICLES = "/vehicles"
+}
