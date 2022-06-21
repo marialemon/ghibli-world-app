3413b9cc82c07c81856fd2cb3cd68b1e867f282d
maria.nunez <maria.nunez@wefox.com>
2022-06-21 21:41:55 +0200

    setup recyclerview

--- app/src/main/java/com/marianunez/ghibliworld/ui/FirstFragment.kt
+++ app/src/main/java/com/marianunez/ghibliworld/ui/FirstFragment.kt
@@ -1,33 +1,43 @@
 package com.marianunez.ghibliworld.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.marianunez.ghibliworld.R
-import com.marianunez.ghibliworld.databinding.FragmentFirstBinding
+import com.marianunez.ghibliworld.databinding.FragmentFilmsListBinding
+import com.marianunez.ghibliworld.ui.adapter.FilmsListAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentFilmsListBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var viewModel: FilmsListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentFilmsListBinding.inflate(inflater, container, false)
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        //esta es la forma nativa que ofrece android de obtener los viewmodels, sino se haría con di
+        viewModel = ViewModelProvider(this)[FilmsListViewModel::class.java]
+
+        initUI()
+
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,6 +45,23 @@ class FirstFragment : Fragment() {
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            viewModel.getFilmsList()
+        }
+    }
+
+    private fun initUI() {
+        recyclerView = binding.filmsList
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = FilmsListAdapter()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            withContext(Dispatchers.IO) {
+                viewModel.getFilmsList()
+            }
+        }
+
+        viewModel.filmsList.observe(viewLifecycleOwner) { list ->
+            (recyclerView.adapter as FilmsListAdapter).setData(list)
         }
     }
 
