8096a591ba2c1c9f61569c62cb0fba025c41c134
maria.nunez <maria.nunez@wefox.com>
2022-06-21 19:15:36 +0200

    create fake data

---
+++ app/src/test/java/com/marianunez/ghibliworld/fakedata/FakeDataApiResponse.kt
@@ -0,0 +1,25 @@
+package com.marianunez.ghibliworld.fakedata
+
+import com.marianunez.ghibliworld.data.network.response.FilmsResponse
+
+val fakeFilmsResponse = FilmsResponse(
+    id = "",
+    title = "",
+    image = "",
+    movieBanner = "",
+    description = "",
+    director = "",
+    releaseDate = "",
+    rtScore = "",
+    people = listOf(),
+    species = listOf(),
+    locations = listOf(),
+    vehicles = listOf(),
+)
+
+val fakeFilmsListResponse = listOf(
+    fakeFilmsResponse,
+    fakeFilmsResponse,
+    fakeFilmsResponse,
+    fakeFilmsResponse
+)
