68495f4310c47818d362040ba03f58c541e8dab1
maria.nunez <maria.nunez@wefox.com>
2022-06-21 19:19:12 +0200

    create viewModel

---
+++ app/src/main/java/com/marianunez/ghibliworld/ui/FilmsListViewModel.kt
@@ -0,0 +1,30 @@
+package com.marianunez.ghibliworld.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.marianunez.ghibliworld.data.datasource.cloud.FilmsListCloudDataSource
+import com.marianunez.ghibliworld.data.network.ApiFactoryService
+import com.marianunez.ghibliworld.data.repository.FilmsListRepositoryImpl
+import com.marianunez.ghibliworld.domain.FilmsListUseCase
+import com.marianunez.ghibliworld.domain.model.FilmsModel
+import kotlinx.coroutines.launch
+
+class FilmsListViewModel : ViewModel() {
+
+    //this is WHY we use Koin for di
+    private val filmsListUseCase = FilmsListUseCase(
+        filmsListRepository = FilmsListRepositoryImpl(
+            filmsListCloudDataSource = FilmsListCloudDataSource(
+                apiFactoryService = ApiFactoryService())))
+
+    private val _filmsList = MutableLiveData<List<FilmsModel>>()
+    val filmsList: LiveData<List<FilmsModel>> = _filmsList
+
+    fun getFilmsList() {
+        viewModelScope.launch {
+            _filmsList.postValue(filmsListUseCase.getFilmsList())
+        }
+    }
+}
