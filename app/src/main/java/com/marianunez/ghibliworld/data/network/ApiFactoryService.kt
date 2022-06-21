0313657d4f127a870a0af6ec82887b7d0fc25e9a
maria.nunez <maria.nunez@wefox.com>
2022-06-21 18:51:39 +0200

    add httpLoggingInterceptor

--- app/src/main/java/com/marianunez/ghibliworld/data/network/ApiFactoryService.kt
+++ app/src/main/java/com/marianunez/ghibliworld/data/network/ApiFactoryService.kt
@@ -1,14 +1,24 @@
 package com.marianunez.ghibliworld.data.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiFactoryService {
 
     fun <T> createApi(apiClass: Class<T>): T {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(ApiConstants.BASE_URL)
+            .client(client)
             .build() // crea la clase retrofit
             .create(apiClass) // esto crea la implementación de la interfaz que se le pasa
     }
