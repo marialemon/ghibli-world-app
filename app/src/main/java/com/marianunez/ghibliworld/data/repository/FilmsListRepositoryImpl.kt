6c46af569d5e332535d95186bb70de7ed4b209a9
maria.nunez <maria.nunez@wefox.com>
2022-06-21 19:18:23 +0200

    create repositoryImpl

---
+++ app/src/main/java/com/marianunez/ghibliworld/data/repository/FilmsListRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.marianunez.ghibliworld.data.repository
+
+import com.marianunez.ghibliworld.data.datasource.cloud.FilmsListCloudDataSource
+import com.marianunez.ghibliworld.data.network.ApiFactoryService
+import com.marianunez.ghibliworld.data.network.response.FilmsResponse
+
+class FilmsListRepositoryImpl(
+    private val filmsListCloudDataSource: FilmsListCloudDataSource = FilmsListCloudDataSource(ApiFactoryService())
+) :
+    FilmsListRepository {
+
+    /* private val filmsListCloudDataSource =
+          FilmsListCloudDataSource(apiFactoryService = ApiFactoryService()) */
+
+    override suspend fun getFilmsList(): List<FilmsResponse> {
+        return filmsListCloudDataSource.getFilmsList()
+    }
+}
