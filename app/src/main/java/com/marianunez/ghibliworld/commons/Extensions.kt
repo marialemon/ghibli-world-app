bf312a916cec04eb100d59b08a18546cf94a5e86
maria.nunez <maria.nunez@wefox.com>
2022-06-21 21:40:25 +0200

    remove unused imports

--- app/src/main/java/com/marianunez/ghibliworld/commons/Extensions.kt
+++ app/src/main/java/com/marianunez/ghibliworld/commons/Extensions.kt
@@ -1,8 +1,5 @@
 package com.marianunez.ghibliworld.commons
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
