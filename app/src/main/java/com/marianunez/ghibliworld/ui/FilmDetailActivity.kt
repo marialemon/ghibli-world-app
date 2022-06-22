11fc573bc8c2bc18dbc88b1f358a92d51eadbdc0
maria.nunez <maria.nunez@wefox.com>
2022-06-22 08:58:50 +0200

    create detail

---
+++ app/src/main/java/com/marianunez/ghibliworld/ui/FilmDetailActivity.kt
@@ -0,0 +1,16 @@
+package com.marianunez.ghibliworld.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.marianunez.ghibliworld.databinding.ActivityFilmDetailBinding
+
+class FilmDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFilmDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFilmDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
