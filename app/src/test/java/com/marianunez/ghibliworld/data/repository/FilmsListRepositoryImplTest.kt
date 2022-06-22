199b05600d5843fce7a8de55eac5060f80d58c84
maria.nunez <maria.nunez@wefox.com>
2022-06-22 15:27:29 +0200

    test repositoryImpl

---
+++ app/src/test/java/com/marianunez/ghibliworld/data/repository/FilmsListRepositoryImplTest.kt
@@ -0,0 +1,23 @@
+package com.marianunez.ghibliworld.data.repository
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.marianunez.ghibliworld.data.datasource.cloud.FilmsListCloudDataSource
+import com.marianunez.ghibliworld.fakedata.fakeFilmsListResponse
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class FilmsListRepositoryImplTest {
+    private val filmsListCloudDataSource = mockk<FilmsListCloudDataSource>()
+    private val filmsListRepositoryImpl = FilmsListRepositoryImpl(filmsListCloudDataSource)
+
+    @Test
+    fun get_films_list_OK() = runBlocking {
+        // si no ponemos runBloquing dará un error de que la función tiene que ser suspend
+        coEvery { filmsListCloudDataSource.getFilmsList() } returns fakeFilmsListResponse
+        val actual = filmsListRepositoryImpl.getFilmsList()
+        assertThat(actual).isEqualTo(fakeFilmsListResponse)
+    }
+}
