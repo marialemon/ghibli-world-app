8a0f1d801efc04257aa67006a6b2fbba9548ab8d
maria.nunez <maria.nunez@wefox.com>
2022-06-28 17:24:20 +0200

    refactor mapper

--- app/src/main/java/com/marianunez/ghibliworld/domain/model/mapper/FilmsMapper.kt
+++ app/src/main/java/com/marianunez/ghibliworld/domain/model/mapper/FilmsMapper.kt
@@ -4,5 +4,14 @@ import com.marianunez.ghibliworld.data.network.response.FilmsResponse
 import com.marianunez.ghibliworld.domain.model.FilmsModel
 
 fun FilmsResponse.toModel(): FilmsModel {
-    return FilmsModel(id, title, image, movieBanner, description, director, releaseDate, rtScore)
+    return FilmsModel(
+        id = id,
+        title = title,
+        image = image,
+        movieBanner = movieBanner,
+        description = description,
+        director = director,
+        releaseDate = releaseDate,
+        rtScore = rtScore
+    )
 }
