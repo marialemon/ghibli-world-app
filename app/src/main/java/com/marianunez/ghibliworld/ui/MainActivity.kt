a3cfcf2db710fabd42a272cdf15bdb902aca18b1
maria.nunez <maria.nunez@wefox.com>
2022-08-11 09:40:01 +0200

    add bottom navigation

--- app/src/main/java/com/marianunez/ghibliworld/ui/MainActivity.kt
+++ app/src/main/java/com/marianunez/ghibliworld/ui/MainActivity.kt
@@ -43,7 +43,9 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_films -> true
+            R.id.action_characters -> true
+            R.id.action_favorites -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
